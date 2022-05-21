4b7fffa9708bf3be99484b7fa85e49742a16c56d
Noah <noahjrush@gmail.com>
2022-05-21 00:47:12 -0400

    ino keras pad seq

--- model/predict/bert_model_predict.py
+++ model/predict/bert_model_predict.py
@@ -4,80 +4,80 @@ from tqdm import tqdm, trange
 import ast
 import torch
 import json
-from keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 from pytorch_pretrained_bert import BertForTokenClassification, BertForSequenceClassification, BertTokenizer, BertConfig
 from transformers import BertTokenizerFast
-def out_util(text, tag2idx, model, MAX_LEN):
-    tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased', do_lower_case=False)
-    encodings = tokenizer(sentence, is_split_into_words=True, return_offsets_mapping=True)
-    tokens = tokenizer.convert_ids_to_tokens(encodings.input_ids)
-    return encodings, tokens
+
+# def out_util(text, tag2idx, model, MAX_LEN):
+#     tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased', do_lower_case=False)
+#     encodings = tokenizer(sentence, is_split_into_words=True, return_offsets_mapping=True)
+#     tokens = tokenizer.convert_ids_to_tokens(encodings.input_ids)
+#     return encodings, tokens
     
-    output = out_util_token(tokenizer, sent, tag2idx, model, MAX_LEN)
-    return format_out_util(sent, output)
+#     output = out_util_token(tokenizer, sent, tag2idx, model, MAX_LEN)
+#     return format_out_util(sent, output)
   
 
-def out_util_token(tokenizer, sent, tag2idx, model, MAX_LEN):
+# def out_util_token(tokenizer, sent, tag2idx, model, MAX_LEN):
     
-    '''
-    input: Sentence, BERT trained model, tag mapping
-    output: Tag for each word in the sentence
-    '''
-    b = pad_sequences([tokenizer.convert_tokens_to_ids(sent)],
-                          maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
-    c = [[float(i>0) for i in ii] for ii in b]
+#     '''
+#     input: Sentence, BERT trained model, tag mapping
+#     output: Tag for each word in the sentence
+#     '''
+#     # b = pad_sequences([tokenizer.convert_tokens_to_ids(sent)],
+#     #                       maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+#     c = [[float(i>0) for i in ii] for ii in b]
     
-    model.eval()
-    with torch.no_grad():
-            output = model(torch.tensor(b), token_type_ids=None,
-                               attention_mask=torch.tensor(c))
-            logits = output['logits']
-    d = []
-    logits = logits.detach().cpu().numpy()
-    d.append([list(p) for p in np.argmax(logits, axis=2)])
+#     model.eval()
+#     with torch.no_grad():
+#             output = model(torch.tensor(b), token_type_ids=None,
+#                                attention_mask=torch.tensor(c))
+#             logits = output['logits']
+#     d = []
+#     logits = logits.detach().cpu().numpy()
+#     d.append([list(p) for p in np.argmax(logits, axis=2)])
     
-    dic={}
-    for k in tag2idx.keys():
-        dic[tag2idx[k]] = k
+#     dic={}
+#     for k in tag2idx.keys():
+#         dic[tag2idx[k]] = k
     
-    output = d[0][0]
-    output = [ dic[a] for a in output]
-    return output
+#     output = d[0][0]
+#     output = [ dic[a] for a in output]
+#     return output
 
-def format_out_util(sent, output):
+# def format_out_util(sent, output):
     
-    '''
-    input: Sentence, Tag for each word in the sentence
-    output: List of string locations in the sentence
-    '''
-    print(output)
-    print(sent)
+#     '''
+#     input: Sentence, Tag for each word in the sentence
+#     output: List of string locations in the sentence
+#     '''
+#     print(output)
+#     print(sent)
 
-    i = 0
-    final_result = []
-    while i < len(output):
-        if output[i] in ['B-LOC', 'I-LOC', 'B-ORG', 'I-ORG'] and i < len(sent):
-            print(i)
-            result=sent[i]
-            j = i + 1
-            while j < len(output) and j < len(sent) and output[j] != 'O':
-                print(j)
-                text = sent[j]
-                result = result + ' ' + text
-                j += 1
+#     i = 0
+#     final_result = []
+#     while i < len(output):
+#         if output[i] in ['B-LOC', 'I-LOC', 'B-ORG', 'I-ORG'] and i < len(sent):
+#             print(i)
+#             result=sent[i]
+#             j = i + 1
+#             while j < len(output) and j < len(sent) and output[j] != 'O':
+#                 print(j)
+#                 text = sent[j]
+#                 result = result + ' ' + text
+#                 j += 1
                 
-            result = result.replace(" ##", "")
-            result = result.replace(" ’ ", "’")
-            result = result.replace(" .", ".")
-            result = result.replace(" ,", ",")
-            final_result = final_result + [result]
-            i = j
-        else:
-            i+=1
-    return final_result
+#             result = result.replace(" ##", "")
+#             result = result.replace(" ’ ", "’")
+#             result = result.replace(" .", ".")
+#             result = result.replace(" ,", ",")
+#             final_result = final_result + [result]
+#             i = j
+#         else:
+#             i+=1
+#     return final_result
 
-from cpu_modeling import from_pretrained
+# from cpu_modeling import from_pretrained
 
 def bert_prediction(model, sentence_df):
     
